5f046e2d1edeebcc2df17f6450766e251e79b4cf
jdang4 <jdang@wpi.edu>
2020-05-22 12:57:20 -0400

    verified that factory works without errors showing up at least :)

--- chess/ChessPieceFactory.cpp
+++ chess/ChessPieceFactory.cpp
@@ -2,6 +2,10 @@
 #include "headers/ChessPiece.h"
 #include "headers/ChessPieceDescriptor.h"
 
+ChessPieceFactory::ChessPieceFactory() {}
+
+ChessPieceFactory::~ChessPieceFactory() {}
+
 ChessPiece* ChessPieceFactory::makePiece(ChessPieceDescriptor* des) {
     ChessPiece* piece = new ChessPiece(des);
     return piece;
