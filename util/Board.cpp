38573ef8ee64bbd19de25de9666128e39cf9e5ec
jetaehyun <jetaehyun0@gmail.com>
2020-05-22 16:46:10 -0400

    added comments and TODOs

--- util/Board.cpp
+++ util/Board.cpp
@@ -1,6 +1,17 @@
 #include "headers/Board.h"
 #include "headers/Square.h"
 
+/**
+ * TODO: Hashmap of <Square, Piece>
+ *  - Compare if something exists on Hashmap
+ *  - Check if a square exists on coordinate
+ *  - check what piece is on the square
+ *  - Remove piece
+ *  - check boundaries with square - is it valid?
+ *  - Populate with proper starting coordinates of each unit  
+ */
+
+
 Board::Board() {
     for(int r = 0; r < MAX_ROWS; r++) {
         for (int c = 0; c < MAX_COLUMNS; c++) {
--- util/Square.cpp
+++ util/Square.cpp
@@ -2,6 +2,10 @@
 #include <cstdlib>
 
 #include "headers/Square.h"
+ /**
+  * TODO: row and column
+  * 
+  */
 
 const auto orthoCheck = [](Square* start, Square* end) 
 {
@@ -27,13 +31,11 @@ Square* Square::makeSquare(int r, int c) {
 }
 
 /**
- * Gets the distance between the calling Square to the destination Square
+ * @brief Gets the distance between the calling Square to the destination Square
  * 
- * dest - the Square to get the distance to
- * 
- * return 
- *      the calculated distance between the 2 Squares or -1 if not moving in a straight line
-*/
+ * @param dest the Square to get the distance to
+ * @return int the calculated distance between the 2 Squares or -1 if not moving in a straight line
+ */
 int Square::distanceTo(Square* dest) {
     int distance = -1;
     bool isOrthogonel = orthoCheck(this, dest);
